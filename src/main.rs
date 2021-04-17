9b16825005b49a687b6fe01c67367649b8042690
tothricsaj <ricktothdevelop@gmail.com>
2021-04-17 16:48:28 +0200

    added temprature exchanging functions

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
 fn main() {
-    println!("Hello, world!");
+    println!("Test fahrenheit_to_celsius -> {}", fahrenheit_to_celsius(22.0));
+    println!("Test celsius_to_fahrenheit -> {}", celsius_to_fahrenheit(6.0));
+}
+
+fn fahrenheit_to_celsius(far: f32) -> f32 {
+    let tmp = {far - 32 as f32};
+    tmp as f32  / 1.8000
+}
+
+fn celsius_to_fahrenheit(cel: f32) -> f32 {
+    let tmp = {cel as f32 * 1.8000};
+    tmp + 32 as f32
 }
