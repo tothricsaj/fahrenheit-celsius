df4174a7c474655756f74f9355c9129de6d9eda3
tothricsaj <ricktothdevelop@gmail.com>
2021-04-17 17:01:57 +0200

    added input reading

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,19 @@
+use std::io;
+
 fn main() {
     println!("Test fahrenheit_to_celsius -> {}", fahrenheit_to_celsius(22.0));
     println!("Test celsius_to_fahrenheit -> {}", celsius_to_fahrenheit(6.0));
+
+    println!("Please, select the input type is celsius or fahrenheit (c/f)!");
+
+    let mut temp_type = String::new();
+
+    io::stdin()
+        .read_line(&mut temp_type)
+        .expect("Fail to read!");
+
+    println!("{}", temp_type)
+    
 }
 
 fn fahrenheit_to_celsius(far: f32) -> f32 {
